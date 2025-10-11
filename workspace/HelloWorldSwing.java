0d589e9f12ca25d2848a532fccc33385dcc60269
122wq <106130176+122wq@users.noreply.github.com>
2025-10-11 00:19:28 +0000

    modified:   workspace/HelloWorldSwing.java

--- workspace/HelloWorldSwing.java
+++ workspace/HelloWorldSwing.java
@@ -1,3 +1,5 @@
+//created by Jack Wang on 10/10
+//made a simple use of swing with menu, buttons, and cards
 import javax.swing.*;  
 import java.awt.*;
 import java.awt.event.*;      
@@ -18,10 +20,12 @@ public class HelloWorldSwing {
         //Add the ubiquitous "Hello World" label.
         JLabel label = new JLabel("Hello World");
         JLabel label2 = new JLabel("Greatest Java programmer");
+        //buttons
         JButton button = new JButton("Press Me");
         button.setSize(100,50);
         JButton button2 = new JButton("Press Me2");
         button2.setSize(100,50);
+        //change text when button is pressed
         button2.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
             label.setText("you did it.");
@@ -32,25 +36,31 @@ public class HelloWorldSwing {
             label.setText("Ok Button clicked.");
          }
         });
-        //screens
+        //screens set up for "main" panel
         JPanel cardPanel = new JPanel();
         CardLayout newLayout = new CardLayout();
         cardPanel.setLayout(newLayout);
         
+        //screens
         JPanel p1 = new JPanel();
         JPanel p2 = new JPanel();
         p1.setLayout(new GridLayout(2, 2));
+        //buttons and labels
         p1.add(button);
         p1.add(button2);
         p1.add(label);
         p2.add(label2);
+        //add the subpanels to main panel
         cardPanel.add(p1,"1");
         cardPanel.add(p2, "2");
-        //menu
+        //menubar
         final JMenuBar menuBar = new JMenuBar();
+        //menu itself
         JMenu menu = new JMenu("Menu");
+        //menuitems
         JMenuItem screen1 = new JMenuItem("Screen1");
         screen1.setActionCommand("Screen1");
+        //switch screens when pressed
         screen1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 newLayout.show(cardPanel, "1");
@@ -63,6 +73,7 @@ public class HelloWorldSwing {
                 newLayout.show(cardPanel, "2");
             } 
         });
+        //add the menu items to menu then add the mnu to menu bar
         menu.add(screen1);
         menu.add(screen2);
         menuBar.add(menu);
